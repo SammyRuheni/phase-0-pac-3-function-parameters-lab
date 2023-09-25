f66eab9c8cfec72d7a1f4b784abced59bb6983b3
SammyRuheni <marvinmuthama03@gmail>
2023-09-25 15:59:45 +0300

    completed the assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// Inside index.js
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  // Inside index.js
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  // Inside index.js
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
